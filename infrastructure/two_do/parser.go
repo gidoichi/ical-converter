898b1c9304503de40974dfb7afe192bc77b9f048
Shinichi Yanagido <hakuto1yds@gmail.com>
2024-01-07 09:33:32 +0900

    separate 2do metadata file

--- infrastructure/two_do/parser.go
+++ infrastructure/two_do/parser.go
@@ -1,11 +1,7 @@
-package infrastructure
+package two_do
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/url"
-	"strings"
 	"time"
 
 	ical "github.com/arran4/golang-ical"
@@ -80,55 +76,4 @@ func (r *twoDoRepository) GetICal(source usecase.DataSource) (cal *ical.Calendar
 	}
 
 	return cal, nil
-}
-
-type ActionType int
-
-var (
-	ActionTypeCall    ActionType = 5
-	ActionTypeMessage ActionType = 6
-	ActionTypeMail    ActionType = 7
-	ActionTypeVisit   ActionType = 8
-	ActionTypeURL     ActionType = 9
-	ActionTypeSearch  ActionType = 10
-)
-
-type metadata struct {
-	ActionType  ActionType `json:"actionType"`
-	ActionValue string     `json:"actionValue"`
-	StartDate   int64      `json:"StartDate"`
-}
-
-func parseMetadata(todo component.Todo) (*metadata, error) {
-	prop := todo.GetProperty("X-2DOAPP-METADATA")
-	if prop == nil {
-		return nil, nil
-	}
-
-	raw := prop.Value
-	content := strings.TrimSuffix(strings.TrimPrefix(raw, "<2Do Meta>"), "</2Do Meta>\\n")
-	content, err := url.QueryUnescape(content)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unescape percent-encoding: %w", err)
-	}
-
-	var parsed metadata
-	if err := json.Unmarshal([]byte(content), &parsed); err != nil {
-		return nil, fmt.Errorf("failed to unmarshall json: %w", err)
-	}
-
-	return &parsed, nil
-}
-
-func (m metadata) getURL() (url *string) {
-	if m.ActionType != ActionTypeURL {
-		return nil
-	}
-
-	return &m.ActionValue
-}
-
-func (m metadata) getStartTime() (*time.Time, error) {
-	t := time.Unix(m.StartDate, 0)
-	return &t, nil
 }
