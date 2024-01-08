34719c200305161b56526c1a8ba200a5a3e6977b
Shinichi Yanagido <hakuto1yds@gmail.com>
2024-01-08 13:21:37 +0900

    add edge case tests

--- infrastructure/two_do/parser_test.go
+++ infrastructure/two_do/parser_test.go
@@ -31,6 +31,23 @@ func TestGetICal(t *testing.T) {
 					Value:          "<2Do Meta>%7B%22RecurrenceValue%22%3A0%2C%22RecurrenceEndRepetitionsOrig%22%3A0%2C%22StartDate%22%3A1688601600%2C%22RecurrenceEndRepetitions%22%3A0%2C%22TaskType%22%3A0%2C%22TaskDuration%22%3A0%2C%22RecurrenceType%22%3A0%2C%22StartDayDelay%22%3A0%2C%22isExpandedToShowChildProjects%22%3A0%2C%22IsStarred%22%3A0%2C%22RecurrenceFrom%22%3A0%2C%22RecurrenceEndType%22%3A0%2C%22actionType%22%3A9%2C%22RUID%22%3A%22%22%2C%22actionValue%22%3A%22http%3A%5C%2F%5C%2Fexample.com%5C%2Fpub%5C%2Fcalendars%5C%2Fjsmith%5C%2Fmytime.ics%22%2C%22DisplayOrder%22%3A0%7D</2Do Meta>\\n",
 				}},
 			}}},
+			&ical.VTodo{ComponentBase: ical.ComponentBase{Properties: []ical.IANAProperty{
+				{BaseProperty: ical.BaseProperty{
+					IANAToken: "UID",
+					Value:     "2",
+				}},
+				{BaseProperty: ical.BaseProperty{
+					IANAToken:      "X-2DOAPP-METADATA",
+					ICalParameters: map[string][]string{"SHARE-SCOPE": {"GLOBAL"}},
+					Value:          "<2Do Meta>%7B%22RecurrenceValue%22%3A0%2C%22RecurrenceEndRepetitionsOrig%22%3A0%2C%22RecurrenceEndRepetitions%22%3A0%2C%22TaskType%22%3A0%2C%22TaskDuration%22%3A0%2C%22RecurrenceType%22%3A0%2C%22StartDayDelay%22%3A0%2C%22isExpandedToShowChildProjects%22%3A0%2C%22IsStarred%22%3A0%2C%22RecurrenceFrom%22%3A0%2C%22RecurrenceEndType%22%3A0%2C%22RUID%22%3A%22%22%2C%22DisplayOrder%22%3A0%7D</2Do Meta>\\n",
+				}},
+			}}},
+			&ical.VTodo{ComponentBase: ical.ComponentBase{Properties: []ical.IANAProperty{
+				{BaseProperty: ical.BaseProperty{
+					IANAToken: "UID",
+					Value:     "3",
+				}},
+			}}},
 		},
 	}
 	dataSource.EXPECT().GetICal().Return(&given, nil)
@@ -45,11 +62,11 @@ func TestGetICal(t *testing.T) {
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
-	if len(cal.Components) != 1 {
+	if len(cal.Components) != 3 {
 		t.Errorf("unexpected calendar: %s", cal.Serialize())
 	}
 
-	log.Println("2Do app metadata are parsed")
+	log.Println("Component with 2Do app metadata having StartDate and ActionType are parsed")
 	var (
 		url     *ical.IANAProperty
 		dtstart *ical.IANAProperty
@@ -74,5 +91,24 @@ func TestGetICal(t *testing.T) {
 	}
 	if dtstart.Value != "20230706" {
 		t.Errorf("unexpected DTSTART: %s", dtstart.Value)
+	}
+
+	log.Println("Component with 2Do app metadata not having StartDate and ActionType are parsed")
+	if len(cal.Components[1].UnknownPropertiesIANAProperties()) != 2 {
+		t.Errorf("component does not have 2 properties: %s", cal.Components[1])
+	}
+	if cal.Components[1].UnknownPropertiesIANAProperties()[0].IANAToken != string(ical.ComponentPropertyUniqueId) {
+		t.Errorf("component does not have UID property: %v", cal.Components[1].UnknownPropertiesIANAProperties()[0])
+	}
+	if cal.Components[1].UnknownPropertiesIANAProperties()[1].IANAToken != "X-2DOAPP-METADATA" {
+		t.Errorf("component does not have X-2DOAPP-METADATA property: %v", cal.Components[1].UnknownPropertiesIANAProperties()[1])
+	}
+
+	log.Println("Component without 2Do app metadata are parsed")
+	if len(cal.Components[2].UnknownPropertiesIANAProperties()) != 1 {
+		t.Errorf("component does not have 1 property: %s", cal.Components[2])
+	}
+	if cal.Components[2].UnknownPropertiesIANAProperties()[0].IANAToken != string(ical.ComponentPropertyUniqueId) {
+		t.Errorf("component does not have UID property: %v", cal.Components[2].UnknownPropertiesIANAProperties()[0])
 	}
 }
